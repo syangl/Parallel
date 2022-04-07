afd3635f92e72317f065e8e084d246d6e303434b
syangl <liusiyangliu@126.com>
2022-04-07 21:13:57 +0800

    大幅修改了simd算法，削减了大量指令，simd性能前后提升明显

--- Based_on_ALEX/src/core/alex_nodes.h
+++ Based_on_ALEX/src/core/alex_nodes.h
@@ -1571,10 +1571,7 @@ inline int simd_search(int m, const K &key)
 
   int size = data_capacity_;
   int32x4_t keys = vmovq_n_s32(key);
-
-  int32x4_t m_zero = vmovq_n_s32(0);
   int32x4_t m_load;                 //加载数组4个元素
-  int32x4_t flags = vmovq_n_s32(0); // 比较结果标志
   int bound;
   if (key_slots_[m]>key)//预测位置大于key实际位置
   {
@@ -1584,33 +1581,9 @@ inline int simd_search(int m, const K &key)
       ////从[0:m-4]搜索key，每次检查大小为4的向量keys，从预测位置m开始从右至左搜索以减少搜索次数。
       ////加载数组中的四个key到m_load向量
       m_load = vld1q_s32(key_slots_+j);
-      
-      ////比较结果标志位flags
-      flags = (int32x4_t)vceqq_s32(keys, m_load);
-      
-      ////令flags向量每个元素为0或1
-      flags = vsubq_s32(m_zero, flags);
-      
-      ////调换flags高两位和低两位
-      int32x4_t tmp = vcombine_s32(vget_high_s32(flags), vget_low_s32(flags));
-      ////flags和倒flags相加
-      flags = vaddq_s32(flags, tmp);
-      
-      
-      int32x4_t final_flag = vcombine_s32(vget_low_s32(flags), vget_low_s32(flags));
-      ////把flags的2和3位移至0和1位，再复制给2和3位
-      int32_t tmp_arr[4];
-      tmp_arr[0]=tmp_arr[2]=vgetq_lane_s32(final_flag, 1);///////////////////////////////////不太好
-      tmp_arr[1]=tmp_arr[3]=vgetq_lane_s32(final_flag, 2);
-      tmp = vld1q_s32(tmp_arr);
-      
-      ////flags和移位后的flags相加，结果为1111或0000
-      final_flag = vaddq_s32(final_flag, tmp);
-      
-      ////res标志是否找到key
-      int32_t res = vgetq_lane_s32(final_flag, 3);
-      
-      if (res == 1)
+      m_load = (int32x4_t)vceqq_s32(keys, m_load);
+      int32_t res = vaddvq_s32(m_load);
+      if (res != 0)
       {////key在此向量中，确定其具体位置
         for (int i = j; i < j + 4; i++)
         {
@@ -1636,20 +1609,10 @@ inline int simd_search(int m, const K &key)
     
     for (int j = m; j < size; j += 4)
     {
-
       m_load = vld1q_s32(key_slots_ + j);
-      flags = (int32x4_t)vceqq_s32(keys, m_load);
-      flags = vsubq_s32(m_zero, flags);
-      int32x4_t tmp = vcombine_s32(vget_high_s32(flags), vget_low_s32(flags));
-      flags = vaddq_s32(flags, tmp);
-      int32x4_t final_flag = vcombine_s32(vget_low_s32(flags), vget_low_s32(flags));
-      int32_t tmp_arr[4];
-      tmp_arr[0]=tmp_arr[2]=vgetq_lane_s32(final_flag, 1);
-      tmp_arr[1]=tmp_arr[3]=vgetq_lane_s32(final_flag, 2);
-      tmp = vld1q_s32(tmp_arr);
-      final_flag = vaddq_s32(final_flag, tmp);
-      int32_t res = vgetq_lane_s32(final_flag, 3);
-      if (res == 1)
+      m_load = (int32x4_t)vceqq_s32(keys, m_load);
+      int32_t res = vaddvq_s32(m_load);
+      if (res != 0)
       {
         for (int i = j; i < j + 4; i++)
         {
